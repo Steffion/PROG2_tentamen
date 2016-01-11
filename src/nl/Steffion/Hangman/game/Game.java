5910f85aa3bc99d47705bccbf04e55939ea1f3af
Steffion <steffion@icloud.com>
2016-01-11 15:42:15 +0100

    Added guessedCharacters, printWord() and chooseRandomWord()

--- src/nl/Steffion/Hangman/game/Game.java
+++ src/nl/Steffion/Hangman/game/Game.java
@@ -1,5 +1,64 @@
 package nl.Steffion.Hangman.game;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game {
 
+	private ArrayList<Character>	guessedCharacters;
+	private String					word;
+									
+	public Game() {
+		guessedCharacters = new ArrayList<Character>();
+	}
+
+	public void addGuessedCharacter(Character character) {
+		guessedCharacters.add(character);
+	}
+
+	public void printWord() {
+		for (char character : word.toCharArray()) {
+			if (guessedCharacters.contains(character)) {
+				System.out.print(character);
+			} else {
+				System.out.print(".");
+			}
+		}
+		
+		System.out.println();
+	}
+	
+	public void chooseRandomWord() {
+		// Get all words and put them in an ArrayList.
+		ArrayList<String> words = new ArrayList<String>();
+		File wordsFile = new File("resource" + File.separator + "woordenlijst_12.txt");
+		
+		try {
+			Scanner wordsList = new Scanner(wordsFile);
+			
+			while (wordsList.hasNext()) {
+				words.add(wordsList.nextLine());
+			}
+			
+			wordsList.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getLocalizedMessage() + " Contact the developer for asstiance!");
+			System.exit(1);
+		}
+
+		// Get a random word from the list.
+		Random random = new Random();
+		word = words.get(random.nextInt(words.size() - 1));
+	}
+	
+	public ArrayList<Character> getGuessedCharacters() {
+		return guessedCharacters;
+	}
+	
+	public String getWord() {
+		return word;
+	}
 }
