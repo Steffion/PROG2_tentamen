7428b41bbbdc4b0073f77b04dda97dc69a3ab638
Steffion <steffion@icloud.com>
2016-01-12 00:01:28 +0100

    Added Gallow
    Made to display the Gallow on the screen

---
+++ src/nl/Steffion/Hangman/game/Gallow.java
@@ -0,0 +1,102 @@
+package nl.Steffion.Hangman.game;
+
+public class Gallow {
+	public static void printGallow(int state) {
+		if (state < 0 || state > 10) {
+			System.out.println("Invalid state, state must be between 0 and 10!");
+			return;
+		}
+		
+		switch(state) {
+			case 0:
+				System.out.println("     ");
+				System.out.println("     ");
+				System.out.println("     ");
+				System.out.println("     ");
+				System.out.println("     ");
+				System.out.println("|____");
+				break;
+			case 1:
+				System.out.println("     ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|____");
+				break;
+			case 2:
+				System.out.println("____ ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|____");
+				break;
+			case 3:
+				System.out.println("____ ");
+				System.out.println("|/   ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|____");
+				break;
+			case 4:
+				System.out.println("____ ");
+				System.out.println("|/ | ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|____");
+				break;
+			case 5:
+				System.out.println("____ ");
+				System.out.println("|/ | ");
+				System.out.println("|  O ");
+				System.out.println("|    ");
+				System.out.println("|    ");
+				System.out.println("|____");
+				break;
+			case 6:
+				System.out.println("____ ");
+				System.out.println("|/ | ");
+				System.out.println("|  O ");
+				System.out.println("|  | ");
+				System.out.println("|    ");
+				System.out.println("|____");
+				break;
+			case 7:
+				System.out.println("____ ");
+				System.out.println("|/ | ");
+				System.out.println("|  O ");
+				System.out.println("| /| ");
+				System.out.println("|    ");
+				System.out.println("|____");
+				break;
+			case 8:
+				System.out.println("____ ");
+				System.out.println("|/ | ");
+				System.out.println("|  O ");
+				System.out.println("| /|\\");
+				System.out.println("|    ");
+				System.out.println("|____");
+				break;
+			case 9:
+				System.out.println("____ ");
+				System.out.println("|/ | ");
+				System.out.println("|  O ");
+				System.out.println("| /|\\");
+				System.out.println("| /  ");
+				System.out.println("|____");
+				break;
+			case 10:
+				System.out.println("____ ");
+				System.out.println("|/ | ");
+				System.out.println("|  O ");
+				System.out.println("| /|\\");
+				System.out.println("| / \\");
+				System.out.println("|____");
+				break;
+				
+		}
+	}
+}
