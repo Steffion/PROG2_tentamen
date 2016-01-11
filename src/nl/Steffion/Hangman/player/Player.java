815b604c1434cb1364e0e9292a9a4e6bdf9a0b4f
Steffion <steffion@icloud.com>
2016-01-11 13:34:49 +0100

    Added Game to Player

--- src/nl/Steffion/Hangman/player/Player.java
+++ src/nl/Steffion/Hangman/player/Player.java
@@ -1,16 +1,27 @@
 package nl.Steffion.Hangman.player;
 
+import nl.Steffion.Hangman.game.Game;
+
 public abstract class Player {
-	private String name;
-	
+	private Game	game;
+	private String	name;
+					
+	public Game getGame() {
+		return game;
+	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public abstract void playGame();
+	
+	public void setGame(Game game) {
+		this.game = game;
+	}
 	
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
 }
