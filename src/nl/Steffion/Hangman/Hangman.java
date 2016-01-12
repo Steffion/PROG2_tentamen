c3317d481723f5d821b2d48444bd219d3d106689
Steffion <steffion@icloud.com>
2016-01-12 15:30:24 +0100

    Moved some methods to Match to make it more OO

--- src/nl/Steffion/Hangman/Hangman.java
+++ src/nl/Steffion/Hangman/Hangman.java
@@ -1,90 +1,23 @@
 package nl.Steffion.Hangman;
 
-import java.util.Random;
-
-import nl.Steffion.Hangman.player.PlayerComputer;
-import nl.Steffion.Hangman.player.PlayerHuman;
-
 public class Hangman {
-
+	
 	private Match match;
-
+	
 	public Hangman() {
 		match = new Match();
 	}
-
+	
 	public void play() {
 		System.out.println("Welkom bij Galgje!");
 		System.out.println();
-
-		ConsoleIO io = new ConsoleIO();
 		
-		/*
-		 * Ask name of first player, or initiate as computer player.
-		 */
-		System.out.print("Geef de naam van speler 1 (of C voor een computer): ");
-		boolean validAnswerGiven = false;
-		while (!validAnswerGiven) {
-			String answer = io.readInput();
-			
-			if ((answer == null) || answer.isEmpty()) {
-				System.out.println("*** Please enter something! ***");
-				continue;
-			} else if (answer.equalsIgnoreCase("C")) {
-				match.setPlayer1(new PlayerComputer());
-				match.getPlayer1().setName("Computer");
-			} else {
-				match.setPlayer1(new PlayerHuman());
-				match.getPlayer1().setName(answer);
-			}
-			
-			validAnswerGiven = true;
-		}
-		
-		/*
-		 * Ask name of second player, or initiate as computer player.
-		 */
-		System.out.print("Geef de naam van speler 2 (of C voor een computer): ");
-		validAnswerGiven = false;
-		while (!validAnswerGiven) {
-			String answer = io.readInput();
-			
-			if ((answer == null) || answer.isEmpty()) {
-				System.out.println("*** Vul alstublieft iets in! ***");
-				continue;
-			} else if (answer.equalsIgnoreCase("C")) {
-				match.setPlayer2(new PlayerComputer());
-				match.getPlayer2().setName("Computer");
-				
-				if (match.getPlayer1().getName().equals("Computer")) {
-					match.getPlayer1().setName("Computer (1)");
-					match.getPlayer2().setName("Computer (2)");
-				}
-			} else {
-				match.setPlayer2(new PlayerHuman());
-				match.getPlayer2().setName(answer);
-			}
-			
-			validAnswerGiven = true;
-		}
-		
-		/*
-		 * Choose random player to begin.
-		 */
-		Random random = new Random();
-		
-		System.out.print("De loting is verricht: ");
-		if (random.nextBoolean()) {
-			System.out.println(match.getPlayer1().getName() + " begint.");
-			match.getPlayer1().playGame();
-		} else {
-			System.out.println(match.getPlayer2().getName() + " begint.");
-			match.getPlayer2().playGame();
-		}
+		match.init();
+		match.start();
 	}
-
-	public void playAgain() {
 	
-	}
+	public void playAgain() {
 
+	}
+	
 }
--- src/nl/Steffion/Hangman/Match.java
+++ src/nl/Steffion/Hangman/Match.java
@@ -1,24 +1,87 @@
 package nl.Steffion.Hangman;
 
+import java.util.Random;
+
 import nl.Steffion.Hangman.player.Player;
+import nl.Steffion.Hangman.player.PlayerComputer;
+import nl.Steffion.Hangman.player.PlayerHuman;
 
 public class Match {
 	private Player	player1;
 	private Player	player2;
-					
-	public Player getPlayer1() {
-		return player1;
+
+	public void init() {
+		ConsoleIO io = new ConsoleIO();
+
+		/*
+		 * Ask name of first player, or initiate as computer player.
+		 */
+		System.out.print("Geef de naam van speler 1 (of C voor een computer): ");
+		while (true) {
+			String answer = io.readInput();
+
+			if ((answer == null) || answer.isEmpty()) {
+				System.out.println("*** Please enter something! ***");
+				continue;
+			} else if (answer.equalsIgnoreCase("C")) {
+				player1 = new PlayerComputer();
+				player1.setName("Computer");
+			} else {
+				player1 = new PlayerHuman();
+				player1.setName(answer);
+			}
+			
+			break;
+		}
+
+		/*
+		 * Ask name of second player, or initiate as computer player.
+		 */
+		System.out.print("Geef de naam van speler 2 (of C voor een computer): ");
+		while (true) {
+			String answer = io.readInput();
+
+			if ((answer == null) || answer.isEmpty()) {
+				System.out.println("*** Vul alstublieft iets in! ***");
+				continue;
+			} else if (answer.equalsIgnoreCase("C")) {
+				player2 = new PlayerComputer();
+				player2.setName("Computer");
+
+				if (player1.getName().equals("Computer")) {
+					player1.setName("Computer (1)");
+					player2.setName("Computer (2)");
+				}
+			} else {
+				setPlayer2(new PlayerHuman());
+				player2.setName(answer);
+			}
+			
+			break;
+		}
 	}
-	
-	public Player getPlayer2() {
-		return player2;
-	}
-	
+
 	public void setPlayer1(Player player1) {
 		this.player1 = player1;
 	}
 	
 	public void setPlayer2(Player player2) {
 		this.player2 = player2;
+	}
+
+	public void start() {
+		/*
+		 * Choose random player to begin.
+		 */
+		Random random = new Random();
+		
+		System.out.print("De loting is verricht: ");
+		if (random.nextBoolean()) {
+			System.out.println(player1.getName() + " begint.");
+			player1.playGame();
+		} else {
+			System.out.println(player1.getName() + " begint.");
+			player1.playGame();
+		}
 	}
 }
