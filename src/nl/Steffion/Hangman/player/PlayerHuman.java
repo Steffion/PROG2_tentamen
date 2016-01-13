738d3b27f31bb2843d2c50096fbade8f2117e7f4
Steffion <steffion@icloud.com>
2016-01-13 10:24:54 +0100

    Formatter over PlayerHuman

--- src/nl/Steffion/Hangman/player/PlayerHuman.java
+++ src/nl/Steffion/Hangman/player/PlayerHuman.java
@@ -4,24 +4,23 @@ import nl.Steffion.Hangman.ConsoleIO;
 import nl.Steffion.Hangman.game.Game;
 
 public class PlayerHuman extends Player {
-
+	
 	@Override
 	public void playGame() {
 		Game game = getGame();
 		game.chooseRandomWord();
-
 		ConsoleIO io = new ConsoleIO();
-
-		while (game.getAmountOfWrongGuesses() < 10 && !game.wordHasBeenGuessed()) {
+		
+		while ((game.getAmountOfWrongGuesses() < 10) && !game.wordHasBeenGuessed()) {
 			game.printWord();
 			game.printWrongGuesses();
 			game.printGallow();
-			
-			System.out.print("Geef de letter die je wilt raden: ");
 
+			System.out.print("Geef de letter die je wilt raden: ");
+			
 			while (true) {
 				String answer = io.readInput();
-				
+
 				if ((answer == null) || answer.isEmpty()) {
 					System.out.println("*** Vul alstublieft iets in! ***");
 					continue;
@@ -32,35 +31,35 @@ public class PlayerHuman extends Player {
 					System.out.println("*** Vul een geldige letter in! ***");
 					continue;
 				}
-
+				
 				char character = answer.toUpperCase().charAt(0);
-
+				
 				if (game.getGuessedCharacters().contains(character)) {
 					System.out.println("*** Je hebt al eerder deze letter gebruikt! ***");
 					continue;
 				}
-				
+
 				int occurrence = 0;
 				for (char wordCharacter : game.getWord().toCharArray()) {
 					if (wordCharacter == character) {
 						occurrence++;
 					}
 				}
-
-				String dynamicText = (occurrence > 0 ? occurrence + " keer " : "niet ");
-				System.out.println("De letter " + character + " komt " + dynamicText + "in het woord voor.");
 				
+				String dynamicText = (occurrence > 0 ? occurrence + " keer" : "niet");
+				System.out.println("De letter " + character + " komt " + dynamicText + " in het woord voor.");
+
 				try {
 					Thread.sleep(1500);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-						
+				
 				game.addGuessedCharacter(character);
 				break;
 			}
 		}
-		
+
 		if (game.wordHasBeenGuessed()) {
 			System.out.println("Het woord is geraden: " + game.getWord());
 		} else {
@@ -69,12 +68,12 @@ public class PlayerHuman extends Player {
 			game.printGallow();
 			System.out.println("Je hebt te vaak fout gegokt, het woord was: " + game.getWord());
 		}
-		
+
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 }
